fba6a05405cb8f41f922fb32d9434a91e17ddd51
Iryna_Novikova <iris_vs@ukr.net>
2025-06-21 01:21:18 +0300

    header without pictures

---
+++ src/js/header.js
@@ -0,0 +1,18 @@
+const openBtnEl = document.querySelector('[data-action="open"]');
+const closeBtnEl = document.querySelector('[data-action="close"]');
+const burgerMenuEl = document.querySelector('[data-visible]');
+const burgerNavListElm = document.querySelector('[data-menu-nav-list]');
+
+openBtnEl.addEventListener('click', e => {
+  burgerMenuEl.dataset.visible = 'open';
+});
+
+closeBtnEl.addEventListener('click', e => {
+  burgerMenuEl.dataset.visible = 'close';
+});
+
+burgerNavListElm.addEventListener('click', e => {
+    if (e.target.tagName === 'A') { 
+      burgerMenuEl.dataset.visible = 'close';  
+    }
+});
