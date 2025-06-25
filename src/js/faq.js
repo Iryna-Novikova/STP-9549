87ddea8e5b486b931da6aa6a40a3abc177f76e92
ShumyloYana <zolotushko1@gmail.com>
2025-06-25 19:02:19 +0300

    new js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,15 +1,15 @@
-document.querySelectorAll(".faq-header").forEach(header => {
+document.querySelectorAll("[data-faq-header]").forEach(header => {
   header.addEventListener("click", () => {
+    const item = header.closest("[data-faq-item]");
+    const answer = item.querySelector("[data-faq-answer]");
     const icon = header.querySelector(".icon-faq");
-    const answer = header.nextElementSibling;
 
     const isOpen = answer.classList.contains("open");
 
-    // Закриваємо всі
-    document.querySelectorAll(".faq-answer").forEach(a => a.classList.remove("open"));
+       document.querySelectorAll("[data-faq-answer]").forEach(a => a.classList.remove("open"));
     document.querySelectorAll(".icon-faq").forEach(i => i.classList.remove("open"));
 
-    if (!isOpen) {
+       if (!isOpen) {
       answer.classList.add("open");
       icon.classList.add("open");
     }
