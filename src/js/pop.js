bd02516bf5b8ad187f258eb14a08a40237e43ae9
Iryna_Novikova <iris_vs@ukr.net>
2025-06-26 19:07:41 +0300

    litle fix

--- src/js/pop.js
+++ src/js/pop.js
@@ -1,35 +1,25 @@
 const popup = document.querySelector('[data-cookies-popup]');
 
-  // Перевірка наявності cookie_status
-  const cookieStatus = localStorage.getItem('cookie_status');
-  if (cookieStatus) {
-    popup.style.display = 'none';
-  }
-
-  // Обробка кнопок
-  document.querySelector('[data-accept-cookies]')
-    .addEventListener('click', () => setCookieStatus('accepted'));
-
-  document.querySelector('[data-decline-cookies]')
-    .addEventListener('click', () => setCookieStatus('declined'));
-
-  function setCookieStatus(status) {
-    localStorage.setItem('cookie_status', status);
-    hideCookiesPopup();
-  }
-
-  function hideCookiesPopup() {
-    popup.style.display = 'none';
+// Перевірка наявності cookie_status
+const cookieStatus = localStorage.getItem('cookie_status');
+if (cookieStatus) {
+  hideCookiesPopup();
 }
-  
-// document
-//   .querySelector('[data-accept-cookies]')
-//   .addEventListener('click', hideCookiesPopup);
-// document
-//   .querySelector('[data-decline-cookies]')
-//   .addEventListener('click', hideCookiesPopup);
 
-// function hideCookiesPopup() {
-//   const popup = document.getElementById('[data-cookies-popup]');
-//   popup.style.display = 'none';
-// }
+// Обробка кнопок
+document
+  .querySelector('[data-accept-cookies]')
+  .addEventListener('click', () => setCookieStatus('accepted'));
+
+document
+  .querySelector('[data-decline-cookies]')
+  .addEventListener('click', () => setCookieStatus('declined'));
+
+function setCookieStatus(status) {
+  localStorage.setItem('cookie_status', status);
+  hideCookiesPopup();
+}
+
+function hideCookiesPopup() {
+  popup.style.display = 'none';
+}
