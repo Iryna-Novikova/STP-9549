c3b6a65ac560eb75b7b80120c8c09520d7610701
ShumyloYana <zolotushko1@gmail.com>
2025-06-26 17:23:58 +0300

    add js pop delete animation faq

--- src/js/pop.js
+++ src/js/pop.js
@@ -1,11 +1,35 @@
-document
-  .querySelector('[data-accept-cookies]')
-  .addEventListener('click', hideCookiesPopup);
-document
-  .querySelector('[data-decline-cookies]')
-  .addEventListener('click', hideCookiesPopup);
+const popup = document.querySelector('[data-cookies-popup]');
 
-function hideCookiesPopup() {
-  const popup = document.getElementById('[data-cookies-popup]');
-  popup.style.display = 'none';
+  // Перевірка наявності cookie_status
+  const cookieStatus = localStorage.getItem('cookie_status');
+  if (cookieStatus) {
+    popup.style.display = 'none';
+  }
+
+  // Обробка кнопок
+  document.querySelector('[data-accept-cookies]')
+    .addEventListener('click', () => setCookieStatus('accepted'));
+
+  document.querySelector('[data-decline-cookies]')
+    .addEventListener('click', () => setCookieStatus('declined'));
+
+  function setCookieStatus(status) {
+    localStorage.setItem('cookie_status', status);
+    hideCookiesPopup();
+  }
+
+  function hideCookiesPopup() {
+    popup.style.display = 'none';
 }
+  
+// document
+//   .querySelector('[data-accept-cookies]')
+//   .addEventListener('click', hideCookiesPopup);
+// document
+//   .querySelector('[data-decline-cookies]')
+//   .addEventListener('click', hideCookiesPopup);
+
+// function hideCookiesPopup() {
+//   const popup = document.getElementById('[data-cookies-popup]');
+//   popup.style.display = 'none';
+// }
