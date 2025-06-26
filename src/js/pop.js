37be712cfb50fb64b046c00c420465069acdd7b8
Iryna_Novikova <iris_vs@ukr.net>
2025-06-26 11:05:09 +0300

    upd

--- src/js/pop.js
+++ src/js/pop.js
@@ -1,7 +1,11 @@
-// document.getElementById('accept-cookies').addEventListener('click', hideCookiesPopup);
-// document.getElementById('decline-cookies').addEventListener('click', hideCookiesPopup);
+document
+  .getElementById('accept-cookies')
+  .addEventListener('click', hideCookiesPopup);
+document
+  .getElementById('decline-cookies')
+  .addEventListener('click', hideCookiesPopup);
 
-// function hideCookiesPopup() {
-//   const popup = document.getElementById('cookies-popup');
-//   popup.style.display = 'none';
-// }
+function hideCookiesPopup() {
+  const popup = document.getElementById('cookies-popup');
+  popup.style.display = 'none';
+}
